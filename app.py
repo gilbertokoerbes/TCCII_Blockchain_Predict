615ca4b1512a90da58e3a1be69f562a2058bc999
gilbertokoerbes <gilbertokoerbes@gmail.com>
2024-04-15 01:19:59 -0300

    App inicial com entradas

--- app.py
+++ app.py
@@ -4,24 +4,7 @@ import pandas as pd
 import plotly.graph_objects as go
 import numpy as np
 import yaml
-
-
-# # Plotando o gráfico
-
-# plt.plot([0, x_period], [0, custo_total], label='Custo Total em Milhares', color='orange', marker='o')
-# plt.plot([0, x_period], [0, blockchain_size_gb], label='BlockChain em GB', color='palevioletred', marker='o')
-
-
-# plt.xlabel('Periodo em anos')
-# plt.ylabel('Predição')
-# plt.title('Gráfico do Custo em relação a Periodo')
-# plt.legend()
-# plt.show()
-
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-
+import predict
 
 
 app = Dash(__name__)
@@ -31,72 +14,30 @@ colors = {
     'text': '#7FDBFF'
 }
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-# df = pd.DataFrame({
-#     "Period": [x_period],
-#     "Cost": [custo_total],
-#     #"City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-# })
+blockchain = predict.blockchain_predict()
+cost = predict.cost_predict(blockchain.blockchain_size_gb)
 
-# fig = px.line(df, x="Period", y="Cost"  )#color="City" barmode="group"
 
-CONF = None
-with open('conf.yml', 'r') as file:
-    CONF = list(yaml.safe_load_all(file))[0]
-#print(CONF)
+t_fields_config = blockchain.transation_fields
 
-###############################################################################################
-
-t_fields_config = CONF['TransactionFields']
-t_size = t_fields_config['H4'] + t_fields_config['S4'] + t_fields_config['P4'] + t_fields_config['R4'] + t_fields_config['E4']
 
 t_fields_hmtl = []
 for field, value in t_fields_config.items():
     input_field = dcc.Input(id=field, value=value, type='number', style={'width': '50%'})
     t_fields_hmtl.append(html.Div([field, input_field], style={'width': '8%','display': 'inline-block', 'margin-right': '0px'}))  # Use CSS para exibir os campos lado a lado
 
-tps               = CONF['BlockchainParameters']['TPS']
-batch_timeout     = CONF['BlockchainParameters']['BatchTimeout']
-max_message_count = CONF['BlockchainParameters']['BatchSize']['MaxMessageCount']
 
-total_transactions = tps * batch_timeout
-if total_transactions > max_message_count:
-    print('Undefined: total_transactions > max_message_count')
-    print('Total_transactions : ', total_transactions)
-   
+#print(t_fields_hmtl)
 
-###############################################################################################
+x_period = np.arange(blockchain.x_period)
+y_predict = []
+for x in x_period:
+    print(x, blockchain.batch_timeout, blockchain.block_size, blockchain.genesis_block_size)
+    y_predict.append(blockchain.calculate_blockchain_size(x, blockchain.batch_timeout, blockchain.block_size, blockchain.genesis_block_size))
+print(x_period)
+print(y_predict)
 
-absolute_max_bytes = CONF['BlockchainParameters']['BatchSize']['AbsoluteMaxBytes']
-size_total_transacoes = total_transactions * t_size
-
-if size_total_transacoes > absolute_max_bytes:
-    print('Undefined: size_total_transacoes > absolute_max_bytes')
-    print('size_total_transacoes : ', size_total_transacoes)
-    
-###############################################################################################
-
-block_header_size   = CONF['BlockchainParameters']['Block']['HeaderSize']
-block_metadata_size = CONF['BlockchainParameters']['Block']['MetadataSize']
-
-block_size = block_header_size + block_metadata_size + size_total_transacoes
-
-###############################################################################################
-
-x_period = CONF['Predict']
-genesis_block_size =  CONF['BlockchainParameters']['GenesisBlockSize']
-
-blockchain_size = ((x_period * 31536000) / batch_timeout) * block_size + genesis_block_size
-
-blockchain_size_gb = blockchain_size / (1024 ** 3)
-###############################################################################################
-
-print(t_fields_hmtl)
-
-x = np.arange(10)
-
-fig = go.Figure(data=go.Scatter(x=x, y=x**2))
+fig = go.Figure(data=go.Scatter(x=x_period, y=y_predict))
 
 fig.update_layout(
     plot_bgcolor=colors['background'],
@@ -114,22 +55,31 @@ app.layout = html.Div([
     html.Div([
 
         html.Div(["TPS", 
-                  dcc.Input(id="tps", value=tps, type='number', 
+                  dcc.Input(id="tps", value=blockchain.tps, type='number', 
                                 style={'width': '50%', 'margin-left': '1%'})],                                
                     style={'width': '8%','display': 'inline-block', 'margin-right': '0px'}),
 
         html.Div(["BatchTimeout (s)", 
-                dcc.Input(id="batch_timeout", value=batch_timeout, type='number', 
+                dcc.Input(id="batch_timeout", value=blockchain.batch_timeout, type='number', 
                                 style={'width': '40%', 'margin-left': '1%'})],
                     style={'width': '20%','display': 'inline-block', 'margin-right': '0px'}),
             
         html.Div(["BatchSize:MaxMessageCount", 
-                dcc.Input(id="max_message_count", value=max_message_count, type='number', 
+                dcc.Input(id="max_message_count", value=blockchain.max_message_count, type='number', 
                                 style={'width': '40%', 'margin-left': '1%'})],
                     style={'width': '35%','display': 'inline-block', 'margin-right': '0px'})
 
     ],
     style={}), #'border-style': 'dotted dashed solid double'
+    html.Br(),
+    html.Div([
+
+        html.Div(["Period", 
+                  dcc.Input(id="period", value=blockchain.x_period, type='number', 
+                                style={'width': '50%', 'margin-left': '1%'})],                                
+                    style={'width': '8%','display': 'inline-block', 'margin-right': '0px'}),
+    ],
+    style={}),
     html.Br(),
     html.Br(),
 
@@ -145,4 +95,5 @@ app.layout = html.Div([
 ])
 
 if __name__ == '__main__':
+
     app.run(debug=True)
