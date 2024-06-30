3610172a23e930d3553c6e3f85fd4ffb19fe84f4
gilbertokoerbes <gilbertokoerbes@gmail.com>
2024-06-30 23:56:01 +0000

    Revert "alterações start.sh para localhost"
    This reverts commit 1c59e370732c4a20540604f42782061263b7ac7d.

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#DNS='blockchain-predict-67a6f0f5b7872efa.elb.us-east-1.amazonaws.com' # LoadBalancer
-DNS='localhost'
+DNS='blockchain-predict-67a6f0f5b7872efa.elb.us-east-1.amazonaws.com' # LoadBalancer
+#DNS='localhost'
 
 docker compose up --build -d --wait	
 
@@ -9,4 +9,4 @@ docker cp jupyter_lab:/home/jovyan/.local/share/jupyter/runtime/ ./runtime
 token=$(jq --raw-output '.token' ./runtime/jpserver-*.json)
 
 echo 'Link de acesso:'
-echo http://$DNS:8888/notebooks/predict.ipynb?token=$token
+echo http://$DNS:80/notebooks/predict.ipynb?token=$token
